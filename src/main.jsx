b345aa5761acdbf2e94599e2eeb9cf4e8eaf1596
Priya Kushawaha <priyakushawaha73@gmail.com>
2024-08-20 14:34:51 +0545

    data fetch

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import {ApolloClient,ApolloProvider, InMemoryCache} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://rickandmortyapi.com/graphql",
+  cache: new InMemoryCache()
+})
 
 //https://rickandmortyapi.com/graphql
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ApolloProvider client = {client}>
     <App />
+    </ApolloProvider>
   </StrictMode>,
 )
---
+++ src/pages/CharacterList.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useQuery ,gql} from '@apollo/client'
+import '../css/characterlist.css'
+const GET_CHARACTERS =gql`
+    query {
+        characters{
+            results{
+            id
+            name
+            image
+            }
+        }
+    }
+`;
+
+function CharacterList() {
+const {error , data, loading} = useQuery(GET_CHARACTERS)
+
+console.log({error , data, loading});
+if (loading) return <div>Spinner....</div>
+
+if (error) return <div>Something went wrong.</div>
+
+
+  return (
+    <div className='CharacterList'>
+      {data.characters.results.map((character )=> {
+
+        return(
+            <div>
+                <img src = {character.image} />
+                <h2>{character.name}</h2>
+                </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default CharacterList
